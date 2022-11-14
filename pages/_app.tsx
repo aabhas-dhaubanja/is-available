0584c7536d2323a1e902169287d66d450f46c54c
Aabhas Dhaubanja <aabhas41@gmail.com>
2022-11-14 08:13:07 +0545

    feat: add dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider, Global } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: "Greycliff, sans-serif",
+        headings: {
+          fontFamily: "Greycliff 2, sans-serif",
+        },
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Global
+        styles={(theme) => [
+          {
+            "@font-face": {
+              fontFamily: "Greycliff",
+              src: "url('/bold.woff2')",
+              fontWeight: 700,
+              fontStyle: "normal",
+            },
+          },
+          {
+            "@font-face": {
+              fontFamily: "Greycliff 2",
+              src: `url('/heavy.woff2')`,
+              fontWeight: 900,
+              fontStyle: "normal",
+            },
+          },
+          {
+            body: { color: theme.colors.pink[6] },
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
